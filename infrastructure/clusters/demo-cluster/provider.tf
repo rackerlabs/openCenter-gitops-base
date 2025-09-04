d1ed4461bd82f82be26761da2ac8f7a1793be499
Miguel Parada <miguelparada@gmail.com>
2025-09-04 14:38:31 -0500

    remove demo-cluster and add to gitignore

--- infrastructure/clusters/demo-cluster/provider.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket       = "opencenter-dev"
-    key          = "demo-cluster/tfstate/terraform.tfstate"
-    region       = "us-west-2"
-    use_lockfile = true
-    encrypt      = true
-  }
-}
